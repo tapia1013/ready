e973e755d95a536b892b7b3e5946d43e011b51ad
tapia1013 <manbearpig1013@gmail.com>
2021-08-02 18:47:44 -0700

    react basics children props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let name = 'Riah';
+let lastName = 'Tapia';
+let age = 69;
+let isLoggedIn = false;
+let width = 400;
+let height = 300;
+
+
+let styles = {
+  box: {
+    backgroundColor: 'dodgerBlue',
+    color: 'white',
+    textAlign: "center",
+    width: `${width * 3 / 2}px`,
+    height: `${height}px`,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  h1: {
+    backgroundColor: 'black',
+    width: '100px',
+    height: '80px',
+    fontFamily: 'sans-serif',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '.8rem'
+  }
+}
+
+
+const App = () => {
+  // let showAccount = () => {
+  //   return (
+  //     <h1>You are Logged In</h1>
+  //   )
+  // }
+
+  const printAlert = (msg) => {
+    alert(msg)
+  }
+
+  return (
+    <>
+      <Header background='black'>
+        <div style={{}}>Logo</div>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">About Us</a>
+          <a href="#">Contact</a>
+        </nav>
+      </Header>
+
+      <Header background='dodgerBlue'>
+        <div style={{}}>Logo</div>
+      </Header>
+    </>
+  )
+}
+
+const Header = (props) => {
+
+  return (
+    <header style={{
+      background: props.background,
+      color: 'white',
+      padding: '15px 20px',
+    }}>
+      {props.children}
+    </header>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
+
+
